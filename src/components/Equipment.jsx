a0eea2f7880be986f25991df13135ff3a8234423
johnsen12 <johnsenultra446@gmail.com>
2025-04-02 00:31:51 +0800

    add delete functionality

--- src/components/Equipment.jsx
+++ src/components/Equipment.jsx
@@ -9,15 +9,19 @@ import {
    TableCell,
    TableBody,
    Button,
+   IconButton,
+   Alert,
 } from "@mui/material"
 import { supabase } from "../utils/supabase";
 import { useState, useEffect } from "react";
 import { AddEquipment } from "./AddEq";
+import { Trash2Icon } from "lucide-react";
 
 export const Equipment = () => {
 
    const [inventory, setInventory] = useState([])
    const [isOpen, setIsOpen] = useState(false);
+   const [success, setSucces] = useState(false)
 
    const fetchInventory = async () => {
       const { data: equipment_inventory, error} = await supabase
@@ -49,6 +53,31 @@ export const Equipment = () => {
       fetchInventory();
    }
 
+   const handleDelete = async (id) => {
+      
+      const confirmDelete = window.confirm("Are you sure you want to delete this item?")
+      if(!confirmDelete) return;
+
+      try {
+         const { error: supabaseError } = await supabase
+         .from("equipment_inventory")
+         .delete()
+         .eq('id', id)
+         
+         if(supabaseError) throw supabaseError;
+         
+         // Update the local state to remove the deleted item
+         setInventory((prevInvent) => 
+            prevInvent.filter((invent) => invent.id !== id)
+         );
+
+         setSucces(true);
+         setTimeout(() => setSucces(false), 300)
+      } catch (err) {
+         console.error("Delete error", err)
+      }
+   }
+
    return (
       <div className="p-3 ">
          {/*  Header  */}
@@ -64,6 +93,7 @@ export const Equipment = () => {
             </Typography>
             <Box>
                <Button
+                  sx={{ mt: 2 }}
                   variant="contained"
                   onClick={handleOpenAddEquipment}
                >
@@ -73,16 +103,18 @@ export const Equipment = () => {
                { isOpen && <AddEquipment onClose={handleCloseEquipment} /> }
                
             </Box>
+            {success && <Alert severity="success" sx={{ my: 2 }}>Item deleted!</Alert>}
          </Box>
          <TableContainer component={Paper} sx={{ boxShadow: 3, mt: 3 }}>
             <Table>
                <TableHead>
-                  <TableRow>
-                     <TableCell>Equipment Name</TableCell>
-                     <TableCell>Category</TableCell>
-                     <TableCell>Stock</TableCell>
-                     <TableCell>Status</TableCell>
-                     <TableCell>Added At</TableCell>
+                  <TableRow sx={{ fontSize: "2.rem" }}>
+                     <TableCell sx={{ fontWeight: "bold" }}>Equipment Name</TableCell>
+                     <TableCell sx={{ fontWeight: "bold" }}>Category</TableCell>
+                     <TableCell sx={{ fontWeight: "bold" }}>Stock</TableCell>
+                     <TableCell sx={{ fontWeight: "bold" }}>Status</TableCell>
+                     <TableCell sx={{ fontWeight: "bold" }}>Added At</TableCell>
+                     <TableCell sx={{ fontWeight: "bold" }}>Action</TableCell>
                   </TableRow>
                </TableHead>
                <TableBody>
@@ -94,7 +126,13 @@ export const Equipment = () => {
                         <TableCell>{invent.status}</TableCell>
                         <TableCell>
                            {new Date(invent.added_at).toLocaleDateString("en-US")}
-
+                        </TableCell>
+                        <TableCell>
+                           <IconButton
+                              onClick={() => handleDelete(invent.id)}
+                           >
+                              <Trash2Icon size={24} />
+                           </IconButton>
                         </TableCell>
                      </TableRow>
                   ))}
