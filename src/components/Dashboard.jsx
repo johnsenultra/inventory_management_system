f5c1df16e2a0f41548628557583565852d86fa00
johnsen12 <johnsenultra446@gmail.com>
2025-04-24 23:54:18 +0800

    .

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -3,8 +3,8 @@ import { Typography } from "@mui/material"
 export const Dashboard = () => {
    console.log("This is dashboard component!!!")
    return  (
-      <>
+      <main>
          <Typography>This is a dashboard component</Typography>
-      </>
+      </main>
    )
 }
