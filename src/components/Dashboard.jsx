ad03c125f6be03d63c960e4b7469dd55a89ad402
johnsen12 <johnsenultra446@gmail.com>
2025-04-24 23:54:55 +0800

    added a component for dashboard

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -4,7 +4,9 @@ export const Dashboard = () => {
    console.log("This is dashboard component!!!")
    return  (
       <main>
+         <Box>
          <Typography>This is a dashboard component</Typography>
+         </Box>
       </main>
    )
 }
