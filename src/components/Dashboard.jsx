f5644bacd86f4dc0e84e49e696b8e4c06e128cd6
johnsen12 <johnsenultra446@gmail.com>
2025-04-30 00:24:20 +0800

    put the needed element to for displaying the status of equipment

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,12 +1,65 @@
-import { Typography } from "@mui/material"
+import { 
+   Typography,
+   Box,
+   Card,
+   Grid2 as Grid,
+
+} from "@mui/material"
 
 export const Dashboard = () => {
-   console.log("This is dashboard component!!!")
+
+
    return  (
-      <main>
-         <Box>
-         <Typography>This is a dashboard component</Typography>
+      <div className="m-5 border-2 border-dotted">
+         <Box sx={{ mb: 1 }}>
+            <Typography sx={{ fontSize: "1.5rem" }}>Dashboard</Typography>
          </Box>
-      </main>
+         
+         <Box 
+            sx={{
+               display: "flex"
+            }}
+         >
+            <Grid container spacing={3} 
+               sx={{                
+                  // display: "flex",
+                  // alignItems: "center",
+                  // justifyContent: "space-between" 
+               }}
+            >
+               <Grid size={{ xs: 12, md: 4 }}>
+                  <Card variant="outlined"
+                     sx={{ p: 2, boxShadow: 5, width: 275, borderRadius: 3, backgroundColor: "#4453F2" }}
+                  >
+                     <Typography>Total Equipment</Typography>
+                  </Card>
+               </Grid>
+
+               <Grid size={{ xs: 12, md: 4 }}>
+                  <Card variant="outlined"
+                     sx={{ p: 2, boxShadow: 5, width: 275 }}
+                  >
+                     <Typography></Typography>
+                  </Card>
+               </Grid>
+
+               <Grid size={{ xs: 12, md: 4 }}>
+                  <Card variant="outlined"
+                     sx={{ p: 2, boxShadow: 5, width: 275}}
+                  >
+                     
+                  </Card>
+               </Grid>
+            </Grid>
+         </Box>
+      </div>
    )
+};
+
+const StatusCard = ({ title, value, color }) => {
+   <Box 
+      
+   >
+
+   </Box>
 }
