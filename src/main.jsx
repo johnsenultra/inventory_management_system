24f3e1759d45536cb413c2864ad406d34f5a6b19
johnsen12 <johnsenultra446@gmail.com>
2025-04-29 23:56:21 +0800

    import Open Sans font from fontsource

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import "@fontsource/open-sans";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
