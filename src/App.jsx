1bb2800ee1c2cee70c662d5512cfbbe0d6522004
johnsen12 <johnsenultra446@gmail.com>
2025-04-29 23:56:01 +0800

    wrap App component with ThemeProvider for theme support

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,22 @@ import { SideBar } from "./components/SideBar";
 import { Dashboard } from "./components/Dashboard";
 import { Equipment } from "./components/Equipment";
 import "./css/index.css";
+import theme from "./utils/theme"
+import { ThemeProvider } from "@mui/material";
 
 function App() {
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/*" element={<SideBar /> }>
-          <Route index element={<Dashboard />} />
-          <Route path="equipment" element={<Equipment />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/*" element={<SideBar /> }>
+            <Route index element={<Dashboard />} />
+            <Route path="equipment" element={<Equipment />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   )
 }
 
