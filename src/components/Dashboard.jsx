d348553fff1f3c398eb3890bd1a7a144518d9e14
johnsen12 <johnsenultra446@gmail.com>
2025-05-01 08:27:05 +0800

    add a conditional rendering for the component

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -4,7 +4,8 @@ import {
    Card,
    Grid2 as Grid,
    CardContent,
-
+   Alert,
+   LinearProgress
 } from "@mui/material"
 import { useState } from "react";
 import { Icon } from "lucide-react"
@@ -41,54 +42,64 @@ export const Dashboard = () => {
    }, [getTotalEquipmentValue]);
    return  (
       <div className="m-5 border-2 border-dotted">
-         <Box sx={{ mb: 1 }}>
-            <Typography sx={{ fontSize: "1.5rem" }}>Dashboard</Typography>
-         </Box>
+
+         {  isLoading
+            ?
+               <LinearProgress />
+            :
+            <div>
+               <Box sx={{ mb: 1 }}>
+                  <Typography sx={{ fontSize: "1.5rem" }}>Dashboard</Typography>
+               </Box>
+               <Box 
+                  sx={{
+                     display: "flex"
+                  }}
+               >
+                  <Grid container spacing={3} 
+                     sx={{                
+                        // display: "flex",
+                        // alignItems: "center",
+                        // justifyContent: "space-between" 
+                     }}
+                  >
+                     <Grid size={{ xs: 12, md: 4 }}>
+                        <Card variant="outlined"
+                           sx={{ boxShadow: 5, width: 275, borderRadius: 3, color: "#fff", backgroundColor: "#4453F2", }}
+                        >
+                           <CardContent>
+                              <Box sx={{ backgroundColor: "#fff", color: "#000", width: 35, p: "5px", borderRadius: 3   }}>
+                                 <Icon sx={{ p: 2 }} iconNode={crossSquare} size={25}/>
+                              </Box>
+                              { error && ( <Alert severity="error" sx={{ m: 1 }}>{error}</Alert> )}
+                              <Typography sx={{ mt: 1.5, fontSize: 15 }}>Total Equipment</Typography>
+                              <Typography sx={{ ml: 0.9, fontSize: 28, fontWeight: 700}}>{totalEquipment}</Typography>
+                           </CardContent>
+                        </Card>
+                     </Grid>
+
+                     <Grid size={{ xs: 12, md: 4 }}>
+                        <Card variant="outlined"
+                           sx={{ p: 2, boxShadow: 5, width: 275 }}
+                        >
+                           <Typography></Typography>
+                        </Card>
+                     </Grid>
+
+                     <Grid size={{ xs: 12, md: 4 }}>
+                        <Card variant="outlined"
+                           sx={{ p: 2, boxShadow: 5, width: 275}}
+                        >
+                           
+                        </Card>
+                     </Grid>
+                  </Grid>
+               </Box>
+            </div>
+         }
          
-         <Box 
-            sx={{
-               display: "flex"
-            }}
-         >
-            <Grid container spacing={3} 
-               sx={{                
-                  // display: "flex",
-                  // alignItems: "center",
-                  // justifyContent: "space-between" 
-               }}
-            >
-               <Grid size={{ xs: 12, md: 4 }}>
-                  <Card variant="outlined"
-                     sx={{ boxShadow: 5, width: 275, borderRadius: 3, color: "#fff", backgroundColor: "#4453F2", }}
-                  >
-                     <CardContent>
-                        <Box sx={{ backgroundColor: "#fff", color: "#000", width: 35, p: "5px", borderRadius: 3   }}>
-                           <Icon sx={{ p: 2 }} iconNode={crossSquare} size={25}/>
-                        </Box>
-                        
-                        <Typography sx={{ mt: 1.5, fontSize: 15 }}>Total Equipment</Typography>
-                        <Typography sx={{ ml: 0.9, fontSize: 28, fontWeight: 700}}>{totalEquipment}</Typography>
-                     </CardContent>
-                  </Card>
-               </Grid>
-
-               <Grid size={{ xs: 12, md: 4 }}>
-                  <Card variant="outlined"
-                     sx={{ p: 2, boxShadow: 5, width: 275 }}
-                  >
-                     <Typography></Typography>
-                  </Card>
-               </Grid>
-
-               <Grid size={{ xs: 12, md: 4 }}>
-                  <Card variant="outlined"
-                     sx={{ p: 2, boxShadow: 5, width: 275}}
-                  >
-                     
-                  </Card>
-               </Grid>
-            </Grid>
-         </Box>
+         
+         
       </div>
    )
 };
