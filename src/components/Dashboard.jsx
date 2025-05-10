f2dbbdd166dd5750fa0fa899fe60986d8915c6e5
johnsen12 <johnsenultra446@gmail.com>
2025-05-11 01:16:32 +0800

    added a error handling using a conditional rendering when the the data failed to load

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -68,84 +68,92 @@ export const Dashboard = () => {
    return  (
       <div className="m-5">
 
-         {  isLoading
-            ?
+         {}
+         
+
+         {  error ? (
+            <Alert severity="error" sx={{ m: 1 }}>{error}</Alert>
+         )  :
+         
+            isLoading ? (
                <LinearProgress />
-            :
-            <div>
-               { error && ( <Alert severity="error" sx={{ m: 1 }}>{error}</Alert> )}
-               <Box sx={{ mb: 1 }}>
-                  <Typography sx={{ fontSize: "1.5rem" }}>Dashboard</Typography>
-               </Box>
-               <Box 
-                  sx={{
-                     display: "flex"
-                  }}
-               >
-                  <Grid container spacing={4} 
-                     sx={{                
-                        // display: "flex",
-                        // alignItems: "center",
-                        // justifyContent: "space-between" 
+            ) : (
+
+               <div>
+                  { error && ( <Alert severity="error" sx={{ m: 1 }}>{error}</Alert> )}
+                  <Box sx={{ mb: 1 }}>
+                     <Typography sx={{ fontSize: "1.5rem" }}>Dashboard</Typography>
+                  </Box>
+                  <Box 
+                     sx={{
+                        display: "flex"
                      }}
                   >
-                     <Grid size={{ xs: 12, sm: 4 }}>
-                        <Card variant="outlined"
-                           sx={{ boxShadow: 5, width: 275, borderRadius: 3, color: "#fff", backgroundColor: "#4453F2", }}
-                        >
-                           <CardContent>
-                              <Box sx={{ backgroundColor: "#fff", color: "#000", width: 32.3, p: "5px", borderRadius: 3   }}>
-                                 <Icon sx={{ p: 2 }} iconNode={crossSquare} size={22}/>
-                              </Box>
-                              <Typography sx={{ mt: 1.5, fontSize: 15 }}>Total Equipment</Typography>
-                              <Typography sx={{ ml: 0.9, fontSize: 28, fontWeight: 700}}>{totalEquipment}</Typography>
-                           </CardContent>
-                        </Card>
-                     </Grid>
+                     <Grid container spacing={4} 
+                        sx={{                
+                           // display: "flex",
+                           // alignItems: "center",
+                           // justifyContent: "space-between" 
+                        }}
+                     >
+                        <Grid size={{ xs: 12, sm: 4 }}>
+                           <Card variant="outlined"
+                              sx={{ boxShadow: 5, width: 275, borderRadius: 3, color: "#fff", backgroundColor: "#4453F2", }}
+                           >
+                              <CardContent>
+                                 <Box sx={{ backgroundColor: "#fff", color: "#000", width: 32.3, p: "5px", borderRadius: 3   }}>
+                                    <Icon sx={{ p: 2 }} iconNode={crossSquare} size={22}/>
+                                 </Box>
+                                 <Typography sx={{ mt: 1.5, fontSize: 15 }}>Total Equipment</Typography>
+                                 <Typography sx={{ ml: 0.9, fontSize: 28, fontWeight: 700}}>{totalEquipment}</Typography>
+                              </CardContent>
+                           </Card>
+                        </Grid>
 
-                     <Grid size={{ xs: 12, sm: 4 }}>
-                        <Card variant="outlined"
-                           sx={{ p: 2, boxShadow: 5, width: 275 }}
-                        >
-                           <Typography></Typography>
-                        </Card>
-                     </Grid>
+                        <Grid size={{ xs: 12, sm: 4 }}>
+                           <Card variant="outlined"
+                              sx={{ p: 2, boxShadow: 5, width: 275 }}
+                           >
+                              <Typography></Typography>
+                           </Card>
+                        </Grid>
 
-                     <Grid size={{ xs: 12, sm: 4 }}>
-                        <Card variant="outlined"
-                           sx={{ p: 2, boxShadow: 5, width: 400, borderRadius: 3 }}
-                        > 
-                        <Typography sx={{ mb: 2, fontWeight: "bold", fontSize: "1rem" }}>Equipment Status</Typography>
-                           <PieChart 
-                              sx={{
-                                 '& .MuiChartsLegend-root': {
-                                    fontSize: '1rem',
-                                 },
-                              }}
-                              series={[
-                                 {
-                                    data: [
-                                       { id: 0, value: available, label: "Available" },
-                                       { id: 1, value: serviceable, label: "Serviceable" },
-                                       { id: 2, value: notServiceable, label: "Not Serviceable" }
-                                    ],
-                                    highlightScope: { fade: "global", highlight: "item" },
-                                    faded: { innerRadius: 0, additionalRadius: -30, color: "gray" },
-                                 }
-                              ]}
-                              height={200}
-                              width={200}
-                              // slotProps={{
-                              //    legend: {
-                              //       direction: "column"
-                              //    }
-                              // }}
-                           />
-                        </Card>
+                        <Grid size={{ xs: 12, sm: 4 }}>
+                           <Card variant="outlined"
+                              sx={{ p: 2, boxShadow: 5, width: 400, borderRadius: 3 }}
+                           > 
+                           <Typography sx={{ mb: 2, fontWeight: "bold", fontSize: "1rem" }}>Equipment Status</Typography>
+                              <PieChart 
+                                 sx={{
+                                    '& .MuiChartsLegend-root': {
+                                       fontSize: '1rem',
+                                    },
+                                 }}
+                                 series={[
+                                    {
+                                       data: [
+                                          { id: 0, value: available, label: "Available" },
+                                          { id: 1, value: serviceable, label: "Serviceable" },
+                                          { id: 2, value: notServiceable, label: "Not Serviceable" }
+                                       ],
+                                       highlightScope: { fade: "global", highlight: "item" },
+                                       faded: { innerRadius: 0, additionalRadius: -30, color: "gray" },
+                                    }
+                                 ]}
+                                 height={200}
+                                 width={200}
+                                 // slotProps={{
+                                 //    legend: {
+                                 //       direction: "column"
+                                 //    }
+                                 // }}
+                              />
+                           </Card>
+                        </Grid>
                      </Grid>
-                  </Grid>
-               </Box>
-            </div>
+                  </Box>
+               </div>
+            )
          }
          
          
